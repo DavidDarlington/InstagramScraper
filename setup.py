a78a6d8232dde8b4413c04b1e12bffd8276e7e48
rarcega <rarcega@users.noreply.github.com>
2018-08-26 21:27:52 -0400

    Updates version

--- setup.py
+++ setup.py
@@ -12,11 +12,11 @@ if sys.version_info < (3, 2):
 
 setup(
     name='instagram-scraper',
-    version='1.5.38',
+    version='1.5.39',
     description=("instagram-scraper is a command-line application written in Python"
                  " that scrapes and downloads an instagram user\'s photos and videos. Use responsibly."),
     url='https://github.com/rarcega/instagram-scraper',
-    download_url='https://github.com/rarcega/instagram-scraper/tarball/1.5.38',
+    download_url='https://github.com/rarcega/instagram-scraper/tarball/1.5.39',
     author='Richard Arcega',
     author_email='hello@richardarcega.com',
     license='Public domain',
